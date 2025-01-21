3124a12deafcdf9cbcac94d87e244b1080584db3
ertianru <oohpier@gmail.com>
2025-01-21 19:07:19 +0800

    added bankruptcy check

--- WovenMonopoly/board.rb
+++ WovenMonopoly/board.rb
@@ -1,27 +1,30 @@
 class Board
-    attr_accessor :spaces, :players, :dice_rolls, :game_over
-    attr_reader :turn_num
+    attr_accessor :spaces, :players, :dice_rolls
+    attr_reader :turn_num, :properties, :curr_player
 
     def initialize(spaces, players, dice_rolls)
         @spaces = spaces
         @players = players
         @dice_rolls = dice_rolls
         @turn_num = 0
+        @curr_player = @players[@turn_num % @players.length]
     end
 
     def start_game
-        @turn_num = 0
-        until @game_over || turn_num > @dice_rolls.length
-            begin
-                player = @players[@turn_num % @players.length]
-                take_turn(player)
-            rescue BankruptError => e
-                puts "#{player.name} is bankrupt (#{player.money})"
-                @game_over = true
-            end
+        until @curr_player.bankrupt? || turn_num > @dice_rolls.length
+            # begin
+            @curr_player = @players[@turn_num % @players.length]
+            take_turn(@curr_player)
+            # rescue BankruptError => e
+            #     puts "#{player.name} is bankrupt (#{player.money})"
+            #     @game_over = true
+            # end
 
             @turn_num += 1
         end
+        puts "#{@curr_player.name} is bankrupt"
+        puts @curr_player
+        puts 'Game Over'
     end
 
     private
--- WovenMonopoly/player.rb
+++ WovenMonopoly/player.rb
@@ -1,7 +1,8 @@
 require_relative 'bankrupt_error'
 
 class Player
-    attr_accessor :name, :money, :properties, :position
+    attr_accessor :name, :money, :position
+    attr_reader :properties
 
     def initialize(name, money = 16, position = 0)
         @name = name
@@ -11,7 +12,7 @@ class Player
     end
 
     def buy_property(property)
-        raise BankruptError if @money < property.price
+        # raise BankruptError if @money < property.price
 
         @money -= property.price
         @properties << property
@@ -19,13 +20,17 @@ class Player
     end
 
     def pay_rent(property)
-        raise BankruptError if @money < property.rent
+        # raise BankruptError if @money < property.rent
 
         @money -= property.rent
         property.owner.money += property.rent
         puts "#{@name} paid rent to #{property.owner.name}"
         puts "#{@name}"
         puts "#{property.owner}"
+    end
+
+    def bankrupt?
+        money < 0
     end
 
     def to_s
